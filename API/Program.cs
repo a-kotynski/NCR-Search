4f79b9e6d8475e7c4523988bddf1dbab5a2ea5cb
A <a.kotynski@hotmail.com>
2023-11-11 07:00:01 +0100

    added-cors

--- API/Program.cs
+++ API/Program.cs
@@ -10,10 +10,12 @@ builder.Services.AddDbContext<DataContext>(options =>
 {
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+builder.Services.AddCors();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
+app.UseCors(corsPolicyBuilder => corsPolicyBuilder.AllowAnyHeader().AllowAnyMethod().WithOrigins("http://localhost:4200"));
 
 app.UseHttpsRedirection();
 
